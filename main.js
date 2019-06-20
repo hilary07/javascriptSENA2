fb5d076a41a1051a27dcc0a163e19574c11883f7
paola0817 <tplinares@misena.edu.co>
2019-06-20 08:03:36 -0500

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-alert("estamos empezando a usar git")
+// alert("estamos empezando a usar git")
 
-console.log("ahora vamos a subir nuestros archivos")
+// console.log("ahora vamos a subir nuestros archivos")
+var persona={
+	nombre: "zulay",
+	apellido: "fajardo",
+	cedula: "1006981280",
+	edad:15
+}
+if (persona.edad>=18)
+{
+	alert(persona.nombre+" es mayor de edad y tiene "+persona.edad)
+
+}else{
+	alert(persona.nombre+" es menor de edad y tiene "+persona.edad)
+}
+
+
